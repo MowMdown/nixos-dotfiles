914f991994db794436940ca50a206fefb7e727ba
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-02 10:23:45 -0400

    Upload files to "/"

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "NixOS Systems";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
+  let
+    system = "x86_64-linux";
+
+    homeModule = {
+      home-manager = {
+        useGlobalPkgs = true;
+        useUserPackages = true;
+
+        users.ryan = import ./home.nix;
+
+        backupFileExtension = "backup";
+      };
+    };
+
+    mkHost = hostModule:
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          hostModule
+
+          home-manager.nixosModules.home-manager
+          homeModule
+        ];
+      };
+  in
+  {
+    nixosConfigurations = {
+      laptop = mkHost ./hosts/laptop/configuration.nix;
+      virtual = mkHost ./hosts/virtual/configuration.nix;
+    };
+  };
+}
