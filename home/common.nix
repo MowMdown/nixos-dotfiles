ff61e84d3501b2176a419f112592b95534faf6a8
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-02 10:23:59 -0400

    Upload files to "home"

---
+++ home/common.nix
@@ -0,0 +1,52 @@
+{ config, pkgs, ... }:
+
+let
+  secrets = import ../secrets.nix;
+in
+{
+  home = {
+    username = "ryan";
+    homeDirectory = "/home/ryan";
+    stateVersion = "26.05";
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    discord
+    fastfetch
+    gpu-screen-recorder-gtk
+    kdePackages.kcalc
+    nextcloud-client
+    onlyoffice-desktopeditors
+    protonplus
+    tree
+    vim
+    wget
+  ];
+
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user.name = secrets.gitUser;
+      user.email = secrets.gitEmail;
+    };
+  };
+
+  programs.firefox = {
+    enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
+  };
+
+  programs.bash = {
+    enable = true;
+    initExtra = builtins.readFile ./xdg-trash-cli;
+
+    shellAliases = {
+      ff = "clear && fastfetch";
+      nix-switch = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#$(hostname)";
+      nix-test = "sudo nixos-rebuild test --flake ~/nixos-dotfiles#$(hostname)";
+    };
+  };
+}
