ffb4e897232890f19d2c33a8717b2c80a000fe4f
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-04 00:29:11 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     in
     {
       nixosConfigurations = {
-        desktop = mkHost ./hosts/laptop/configuration.nix;
+        desktop = mkHost ./hosts/desktop/configuration.nix;
         laptop = mkHost ./hosts/laptop/configuration.nix;
         virtual = mkHost ./hosts/virtual/configuration.nix;
       };
