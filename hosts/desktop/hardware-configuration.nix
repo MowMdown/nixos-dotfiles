a8478b6bdd1069da14f0629feb5b410b599b08d0
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-04 11:10:16 -0400

    Update hosts/desktop/hardware-configuration.nix

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -64,6 +64,9 @@ in
   }];
 
   hardware.firmware = [ pkgs.wireless-regdb ];
+  hardware.graphics.enable = true;
+  hardware.graphics.enable32Bit = true;
+  
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
