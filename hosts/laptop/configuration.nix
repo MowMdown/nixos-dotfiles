fc6ef566d0e99414f447ed29351ccdf5bd93651b
mowmdown <wallacee.ryan10@gmail.com>
2026-06-06 02:01:45 -0400

    Update networking.firewall fules

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -18,7 +18,11 @@
     networkmanager.enable = true;
     networkmanager.wifi.backend = "iwd";
     nftables.enable = true;
-    firewall.enable = true;
+  };
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 53317 ];
+    allowedUDPPorts = [ 53317 ];
   };
 
   hardware.openrazer.enable = true;
