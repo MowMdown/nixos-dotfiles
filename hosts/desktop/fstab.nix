ca81207682463911947798f088b8fe81254ed472
mowmdown <wallacee.ryan10@gmail.com>
2026-06-07 14:24:29 -0400

    Update desktop UUID

--- hosts/desktop/fstab.nix
+++ hosts/desktop/fstab.nix
@@ -2,7 +2,7 @@ let
   nvme0n1p1 = "/dev/disk/by-uuid/DD83-436F";
   nvme0n1p2 = "/dev/disk/by-uuid/be9582c6-5562-4617-9500-a1d6a6d00ddd";
   nvme1n1p1 = "/dev/disk/by-uuid/bcf7f4cf-647f-46af-835a-7ae162a1972b";
-# nvme1n1p2 = "/dev/disk/by-uuid/";
+# nvme1n1p2 = "/dev/disk/by-uuid/be9582c6-5562-4617-9500-a1d6a6d00ddd";
   sdb1      = "/dev/disk/by-uuid/7599fa7b-6de7-4d2c-9423-6469e1b9d643";
   btrfsOpts = [ "compress=zstd:3" "noatime" "discard=async" "space_cache=v2" ];
 in
