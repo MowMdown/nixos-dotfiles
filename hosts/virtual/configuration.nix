89067522499e21a0eb80edbfd29521ced1de9d02
mowmdown <wallacee.ryan10@gmail.com>
2026-06-07 02:03:22 -0400

    Refactor virtual host configs

--- hosts/virtual/configuration.nix
+++ hosts/virtual/configuration.nix
@@ -1,18 +1,54 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
 {
   imports = [
-    ./hardware-configuration.nix
-    ./tailscale.nix
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./fstab.nix
     ../../modules/common.nix
     ../../modules/upgrade.nix
-    ../../modules/vm.nix
   ];
 
-    networking = {
-      hostName = "virtual";
-      usePredictableInterfaceNames = false;
-      networkmanager.enable = true;
-      nftables.enable = true;
-      firewall.enable = true;
+  boot = {
+    initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "sr_mod" "virtio_blk" ];
+    kernelModules = [ "kvm-amd" ];
+    initrd.kernelModules = [ ];
+    extraModulePackages = [ ];
   };
 
+  networking = {
+    hostName = "virtual";
+    usePredictableInterfaceNames = false;
+    networkmanager.enable = true;
+    nftables.enable = true;
+    firewall = {
+      enable = true;
+      trustedInterfaces = [ "tailscale0" ];
+      allowedUDPPorts = [ config.services.tailscale.port ];
+    };
+  };
+
+  services = {
+    qemuGuest.enable = true;
+    spice-vdagentd.enable = true;
+    networkd-dispatcher = {
+      enable = true;
+      rules."50-tailscale-optimizations" = {
+        onState = [ "routable" ];
+        script = "${pkgs.ethtool}/bin/ethtool -K enp1s0 rx-udp-gro-forwarding on rx-gro-list off";
+      };
+    };
+  };
+
+  systemd.services.tailscaled.serviceConfig.Environment = [
+    "TS_DEBUG_FIREWALL_MODE=nftables"
+  ];
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
