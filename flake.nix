86f3f51e065d3c667ac3b072284707e0e07f9c77
mowmdown <wallacee.ryan10@gmail.com>
2026-06-08 22:45:48 -0400

    Added CachyOS Kernel Support

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixvim.url = "github:nix-community/nixvim/nixos-26.05";
+    cachyos.url = "github:xddxdd/nix-cachyos-kernel";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,7 +14,8 @@
       inputs.home-manager.follows = "home-manager";
     };
   };
-  outputs = inputs@{ self, nixpkgs, home-manager, plasma-manager, nixvim, ... }:
+
+  outputs = inputs@{ self, nixpkgs, home-manager, plasma-manager, nixvim, cachyos, ... }:
     let
       system = "x86_64-linux";
       mkHost = hostModule: nixpkgs.lib.nixosSystem {
@@ -22,6 +24,7 @@
           hostModule
           home-manager.nixosModules.home-manager
           {
+            nixpkgs.overlays = [ cachyos.overlays.default ];
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.ryan = import ./home.nix;
--- modules/common.nix
+++ modules/common.nix
@@ -6,7 +6,8 @@
     loader.limine.maxGenerations = 3;
     loader.efi.canTouchEfiVariables = true;
     kernel.sysctl = { "vm.swappiness" = 100; };
-    kernelPackages = pkgs.linuxPackages_latest;
+    #kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
     kernelParams = [
       "zswap.enabled=1"
       "zswap.compressor=zstd"
