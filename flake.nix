9a8ae2fbb1c66e5bed981a9e8343fea9e62b21dc
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-12 10:33:50 -0400

    Removing home-manager

--- flake.nix
+++ flake.nix
@@ -8,12 +8,8 @@
       url = "git+https://git.plexraid.stream/mowmdown/nixpkgs";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = inputs@{ self, nixpkgs, home-manager, nixvim, cachyos, my-nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, nixvim, cachyos, my-nixpkgs, ... }:
     let
       system = "x86_64-linux";
       mkHost = hostModule: nixpkgs.lib.nixosSystem {
@@ -21,18 +17,11 @@
         modules = [
           hostModule
           my-nixpkgs.nixosModules.my-nixpkgs
-          home-manager.nixosModules.home-manager
+          nixvim.nixosModules.nixvim
           {
             nixpkgs.overlays = [
               cachyos.overlays.default
               my-nixpkgs.overlays.default
-            ];
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.ryan = import ./home.nix;
-            home-manager.backupFileExtension = "backup";
-            home-manager.sharedModules = [
-              nixvim.homeModules.nixvim
             ];
             disabledModules = [ "programs/gpu-screen-recorder.nix" ];
           }
