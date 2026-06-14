245ad9417b65aebf5e28747f018e7cce9a3820e7
mowmdown <mowmdown@git.plexraid.stream>
2026-06-14 14:16:41 -0400

    Added back in system

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -89,7 +89,7 @@ in
     graphics.enable32Bit = true;
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   nix.settings.system-features = [ "gccarch-znver3" "gccarch-x86-64-v3" "gccarch-x86-64-v2" "gccarch-x86-64" ];
-  nix.settings.cores = 16;
-  nix.settings.max-jobs = 16;
 }
