bf3353907047e8bd0c71fe7ca9195e932f014538
mowmdown <mowmdown@noreply.git.plexraid.stream>
2026-06-30 12:14:59 -0400

    Update hosts/desktop/configuration.nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -26,17 +26,17 @@
     '';
     extraModulePackages = [ ];
     loader.limine.extraEntries = ''
-      /Windows 11
-      protocol: efi
-      path: guid(4e4d4d22-81e9-4d8a-9b07-528d0bf1aa59):/EFI/Microsoft/Boot/bootmgfw.efi
-      comment: Windows 11 Pro
-      /Gentoo Linux
-      protocol: linux
-      kernel_path: boot():/kernel-7.0.10-gentoo-r1-custom
-      module_path: boot():/amd-uc.img
-      module_path: boot():/initramfs-7.0.10-gentoo-r1-custom.img
-      cmdline: root=LABEL=ARCH rootflags=subvol=@gentoo rw nowatchdog amdgpu.ppfeaturemask=0xffffffff zswap.enabled=1 zswap.compressor=lz4
-      comment: Linux Kernel 7.0.10-r1
+/Windows 11
+  protocol: efi
+  path: guid(4e4d4d22-81e9-4d8a-9b07-528d0bf1aa59):/EFI/Microsoft/Boot/bootmgfw.efi
+  comment: Windows 11 Pro
+/Gentoo Linux
+  protocol: linux
+  kernel_path: boot():/kernel-7.0.10-gentoo-r1-custom
+  module_path: boot():/amd-uc.img
+  module_path: boot():/initramfs-7.0.10-gentoo-r1-custom.img
+  cmdline: root=LABEL=ARCH rootflags=subvol=@gentoo rw nowatchdog amdgpu.ppfeaturemask=0xffffffff zswap.enabled=1 zswap.compressor=lz4
+  comment: Linux Kernel 7.0.10-r1
     '';
   };
   
